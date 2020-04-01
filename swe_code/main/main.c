b8588708b927a524171c3279b1b9551f484c3421
tcew <tim.warburton@gmail.com>
2020-04-01 16:30:38 -0400

    WIP: tidy up

--- swe_code/main/main.c
+++ swe_code/main/main.c
@@ -334,9 +334,10 @@ int main(int argc, char** argv) {
 
     // hard code OpenCL device
     char occaConfig[BUFSIZ];
-    //sprintf(occaConfig, "mode: 'OpenCL'  , device_id: %d, platform_id: %d", deviceID, platformID);
+    //    sprintf(occaConfig, "mode: 'OpenCL'  , device_id: %d, platform_id: %d", deviceID, platformID);
     sprintf(occaConfig, "mode: 'CUDA'  , device_id: %d, platform_id: %d", deviceID, platformID);
-
+    printf("SWE:occaConfig=%s\n", occaConfig);
+    
     // initialize OCCA device
     device = occaCreateDeviceFromString(occaConfig);
 	
